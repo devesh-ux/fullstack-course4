58ddadbd8502b5baaa7bf4eb7998d8aa06a840da
devesh-ux <65006258+devesh-ux@users.noreply.github.com>
2020-05-08 15:06:32 +0530

    Update script.js

--- assignments/assignment4/assignment4-solution-starter/easier/script.js
+++ assignments/assignment4/assignment4-solution-starter/easier/script.js
@@ -33,6 +33,19 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+
+
+var byeSpeaker = {
+    speak : function(name){
+        console.log("Good Bye " + name);
+    }
+};
+
+var helloSpeaker = {
+    speak : function(name){
+        console.log("Hello " + name);
+    }
+};
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
